3b6ec2381a9e20a9e4b2ca0b70c3d9a028fbd900
小米里的大麦 <hc1960074081@qq.com>
2024-12-08 13:38:26 +0800

    牛客

---
+++ Problem-solving code(刷题代码)/牛客：素数个数.cpp
@@ -0,0 +1,51 @@
+﻿//牛客：素数个数
+//https://ac.nowcoder.com/acm/contest/97845/N
+//参赛密码：517acm
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool is(int x)
+{
+    if (x < 1)                              // 小于1的数不是素数
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= sqrt(x); i++)      // 遍历2到sqrt(x)
+    {
+        if (x % i == 0)                     // 如果x能被i整除，则不是素数，直接返回false
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int count = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (is(i))
+            {
+                count++;
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
