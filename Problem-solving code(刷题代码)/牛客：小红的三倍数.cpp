da1e78b400f8ecc9c16f05301f644b412d90beb6
小米里的大麦 <hc1960074081@qq.com>
2024-12-10 12:55:44 +0800

    牛客

---
+++ Problem-solving code(刷题代码)/牛客：小红的三倍数.cpp
@@ -0,0 +1,40 @@
+﻿//牛客：小红的三倍数
+//https://ac.nowcoder.com/acm/contest/97845/S
+//参赛密码：517acm
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    long long ret = 0;
+
+    for (int i = 0; i < n; ++i) 
+    {
+        string s;
+        cin >> s;
+
+        long long sum = 0;
+        for (char c : s) 
+        {
+            sum += c - '0';
+        }
+
+        ret += sum % 3;
+    }
+
+    if (ret % 3 == 0) 
+    {
+        cout << "YES" << endl;
+    }
+    else 
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
