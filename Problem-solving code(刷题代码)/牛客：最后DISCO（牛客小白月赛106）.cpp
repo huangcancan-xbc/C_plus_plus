4e0bbf60f03df5a6fd8f5de13bc0ab583226184b
小米里的大麦 <hc1960074081@qq.com>
2024-12-19 10:56:46 +0800

    牛客

---
+++ Problem-solving code(刷题代码)/牛客：最后DISCO（牛客小白月赛106）.cpp
@@ -0,0 +1,34 @@
+﻿//牛客：最后DISCO（牛客小白月赛106）
+//https://ac.nowcoder.com/acm/contest/97439/A
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a = 0, b = 0, c = 0, d = 0;
+    cin >> a >> b >> c >> d;
+    long long sum = 1;
+    if (a % 2 == 0)
+    {
+        sum = 2;
+    }
+
+    if (b == 0)
+    {
+        sum = 1;
+    }
+
+    sum=sum*c+d;
+
+    if(sum%2==0)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+
+    return 0;
+}
