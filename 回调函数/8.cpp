20b911a77fa3621ad92297070f9f789f9e825c75
小米里的大麦 <hc1960074081@qq.com>
2025-10-29 14:08:53 +0800

    bind&&占位符

---
+++ 回调函数/8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <functional>
+using namespace std;
+//using namespace std::placeholders;
+
+void show(int a, int b, int c)
+{
+    cout << a << " " << b << " " << c << endl;
+}
+
+// 
+int main()
+{
+    auto f1 = bind(show, std::placeholders::_1, 20, 30);            // 绑定两个参数
+    f1(10);                                                         // show(10,20,30)
+
+    auto f2 = bind(show, placeholders::_2, placeholders::_1, 30);   // 绑定三个参数（注意这里的顺序！）
+    // f2(10);  // 报错！
+    f2(10, 20);                                                     // show(20,10,30)
+    // 补充的顺序默认就是第一个，第二个，第三个...第一个参数会补充给placeholders::_1，第二个参数会补充给placeholders::_2，以此类推
+
+    auto f3 = bind(show, 10, 20, 30);       // 没有占位符
+    f3();                                   // show(10,20,30)
+}
