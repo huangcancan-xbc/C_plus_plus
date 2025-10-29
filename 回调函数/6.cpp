3d84e024658cba90ba4be1d0bee59c4a57e67ca1
小米里的大麦 <hc1960074081@qq.com>
2025-10-29 14:07:45 +0800

    C++11 封装的functional，对库中function的入门使用

---
+++ 回调函数/6.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <functional>
+#include <sstream>
+using namespace std;
+
+void print1()
+{
+	cout << "Hello, World!" << endl;
+}
+void test1(int n, function<void()> callback)
+{
+	for (int i = 0; i < n; i++)
+	{
+		callback();
+	}
+}
+
+
+void print2(string s)
+{
+	cout << s << endl;
+}
+void test2(int n, function<void(string)> callback)
+{
+	for (int i = 0; i < n; i++)
+	{
+		callback("hello, callback!");
+	}
+}
+
+
+void test3(int n, function<void(string)> callback)
+{
+	for (int i = 0; i < n; i++)
+	{
+		stringstream ss;
+		ss << "这是第 " << i << " 次调用";
+		callback(ss.str());
+	}
+}
+
+
+int main()
+{
+	test1(3, print1);
+	test2(3, print2);
+	test3(3, print2);
+
+	return 0;
+}
