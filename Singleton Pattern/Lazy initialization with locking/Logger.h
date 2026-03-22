c50c56931e4a4bbb4bc395f2031a75defec69e86
huangcancan-xbc <hc1960074081@qq.com>
2026-03-22 19:49:30 +0800

    加锁懒汉单例

---
+++ Singleton Pattern/Lazy initialization with locking/Logger.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <mutex>
+
+// 单例（加锁懒汉）
+//懒加载，同时用锁保护第一次创建
+//线程安全
+//每次调用都加锁，性能一般
+//写法麻烦
+//仍然要处理释放问题
+class Logger
+{
+public:
+	static Logger* getInstance();		// 提供访问
+
+	void log(const std::string& msg);	// 日志函数
+
+private:
+
+	Logger();	// 私有构造
+
+	// 禁用拷贝
+	Logger(const Logger&) = delete;
+	Logger& operator=(const Logger&) = delete;
+
+	static Logger* _instance;
+	static std::mutex _mutex;
+};
---
+++ Singleton Pattern/Lazy initialization with locking/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "Logger.h"
+#include <iostream>
+#include <thread>
+
+void create_thread()
+{
+    Logger* log = Logger::getInstance();
+    log->log("线程创建成功！");
+
+    std::cout << "当前 log 实例地址：" << log << std::endl;
+}
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        std::thread p(create_thread);
+        p.join();
+    }
+
+    return 0;
+}
