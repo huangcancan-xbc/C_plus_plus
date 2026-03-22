ce008378fc0f7706fe750369beae2b66f4940f43
huangcancan-xbc <hc1960074081@qq.com>
2026-03-22 19:48:04 +0800

    原始懒汉单例（不安全）

---
+++ Singleton Pattern/Original lazy style/Logger.cpp
@@ -0,0 +1,25 @@
+﻿#include "Logger.h"
+
+#include <iostream>
+
+Logger* Logger::_instance = nullptr;	// 初始化
+
+Logger::Logger()
+{
+	// 简化初始化
+}
+
+Logger* Logger::getInstance()
+{
+	if (_instance == nullptr)
+	{
+		_instance = new Logger();
+	}
+
+	return _instance;
+}
+
+void Logger::log(const std::string& msg)
+{
+	std::cout << "[log] " << msg << std::endl;
+}
---
+++ Singleton Pattern/Original lazy style/Logger.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+
+// 单例（原始懒汉）
+//第一次用的时候再创建对象
+//用到时才创建
+//线程不安全
+//还要考虑释放内存
+class Logger
+{
+public:
+	static Logger* getInstance();		// 提供访问
+
+	void log(const std::string& msg);	// 日志函数
+
+private:
+
+	Logger();	// 私有构造
+
+	// 禁用拷贝
+	Logger(const Logger&) = delete;
+	Logger& operator=(const Logger&) = delete;
+
+	static Logger* _instance;
+};
